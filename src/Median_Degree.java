df2bcc2156c1a61d9851cd289e53a43f8ed975a4
Srivats Jayaram <srivats666@gmail.com>
2016-07-08 21:57:00 -0700

    o(n) quick select

--- src/Median_Degree.java
+++ src/Median_Degree.java
@@ -8,6 +8,7 @@ import java.text.DecimalFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
@@ -196,7 +197,7 @@ public class Median_Degree {
 		}
 	}
 	
-	private void calcMedian(Node input)
+	private void calcMedian()
 	{
 		List<Integer> l = new ArrayList<Integer>();
 		for(Entry<String, Integer> e : map.entrySet())
@@ -205,12 +206,11 @@ public class Median_Degree {
 		}
 		
 		Collections.sort(l);
-		//System.out.println("list " + l);
 		int idx = l.size();
 		
 		if(idx == 0)
 		{
-			prevMedian = 0; 
+			prevMedian = 0.00; 
 		}
 		else if(idx % 2 != 0)
 		{
@@ -220,8 +220,80 @@ public class Median_Degree {
 		{
 			prevMedian = ((double)l.get(idx/2) + l.get((idx - 1)/2))/2; 
 		}
-		
 	}
+	
+	private void calcMedian2()
+	{
+		int n = map.size();
+		int[] nums = new int[n];
+		int i = 0;
+		for(Entry<String, Integer> e : map.entrySet())
+		{
+			nums[i++] = e.getValue();
+		}
+		
+		if(n == 0)
+		{
+			prevMedian = 0.00; 
+		}
+		else
+		{
+			if(n % 2 != 0)
+			{
+				prevMedian = (double)findKthLargest(nums, n/2);
+			}
+			else
+			{
+				int idx1 = findKthLargest(nums, n/2 - 1);
+				int idx = findKthLargest(nums, n/2);
+				prevMedian = ((double)idx + idx1)/2;
+			}
+		}
+	}
+	
+	public int findKthLargest(int[] nums, int k) 
+	{
+        int lo = 0;
+        int hi = nums.length - 1;
+        while (lo < hi) {
+            final int j = partition(nums, lo, hi);
+            if(j < k) {
+                lo = j + 1;
+            } else if (j > k) {
+                hi = j - 1;
+            } else {
+                break;
+            }
+        }
+        return nums[k];
+    }
+
+    private int partition(int[] a, int lo, int hi) {
+
+        int i = lo;
+        int j = hi + 1;
+        while(true) {
+            while(i < hi && less(a[++i], a[lo]));
+            while(j > lo && less(a[lo], a[--j]));
+            if(i >= j) {
+                break;
+            }
+            exch(a, i, j);
+        }
+        exch(a, lo, j);
+        return j;
+    }
+
+    private void exch(int[] a, int i, int j) {
+        final int tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+
+    private boolean less(int v, int w) {
+        return v < w;
+    }
+    
 	
 	public void getMedian(String to, String from, String time)
 	{
@@ -250,7 +322,7 @@ public class Median_Degree {
 		if(prevTime.before(timeObj))
 			removeOldEntries(timeObj);
 		
-		calcMedian(input);
+		calcMedian();
 		
 		if(timeObj.after(prevTime))
 			prevTime = timeObj;
@@ -286,7 +358,7 @@ public class Median_Degree {
 			    String actor = parts[2].split(":")[1].replaceAll("\"", "").replaceAll("}", "");
 				created_time = created_time.replaceAll("\"", "").replaceAll(" ", "");
 				m.getMedian(target, actor, created_time);
-				bw.write("" + m.prevMedian);
+				bw.write(String.format("%.2f",m.prevMedian));
 				bw.newLine();
 			}
 			
